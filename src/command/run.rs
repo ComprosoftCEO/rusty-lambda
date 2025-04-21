2ba3b4992139002d07506ef4115ebe056c6be2e6
Bryan McClain <bamcclain10@gmail.com>
2025-04-21 19:44:54 -0400

    Wrapper for REPL type

--- src/command/run.rs
+++ src/command/run.rs
@@ -25,8 +25,6 @@ pub struct RunArgs {
   files: Vec<PathBuf>,
 }
 
-static ABORT_EXECUTION: AtomicBool = AtomicBool::new(false);
-
 impl RunArgs {
   pub fn execute(self) -> super::CommandResult {
     let text_data = Arena::new();
@@ -56,30 +54,64 @@ impl RunArgs {
       return Ok(());
     }
 
+    Repl::new(&text_data, &executor, self.steps).run()
+  }
+}
+
+struct Repl<'text, 'assign>
+where
+  'text: 'assign,
+{
+  text_data: &'text Arena<String>,
+  executor: &'assign Executor<'assign>,
+  show_steps: bool,
+  abort: &'static AtomicBool,
+}
+
+impl<'text, 'assign> Repl<'text, 'assign>
+where
+  'text: 'assign,
+{
+  pub fn new(text_data: &'text Arena<String>, executor: &'assign Executor<'assign>, show_steps: bool) -> Self {
+    static ABORT_EXECUTION: AtomicBool = AtomicBool::new(false);
+
+    Self {
+      text_data,
+      executor,
+      show_steps,
+      abort: &ABORT_EXECUTION,
+    }
+  }
+
+  pub fn run(mut self) -> super::CommandResult {
+    // Initialize the Ctrl+C handler
     if let Err(e) = ctrlc::set_handler(|| {
-      ABORT_EXECUTION.store(true, Ordering::Relaxed);
+      self.abort.store(true, Ordering::Relaxed);
     }) {
       eprint!("{}failed to set Ctrl+C handler: {}", "Warning: ".yellow(), e);
     }
 
+    // Set up REPL editor
     let mut editor = DefaultEditor::new()?;
     editor.set_auto_add_history(true);
 
-    let mut should_exit = false;
+    // We only want to exit if Ctrl+C pressed twice in a row
+    let mut ctrl_c_should_exit = false;
+
     loop {
       let line = match editor.readline("> ") {
         Ok(line) => {
-          should_exit = false;
+          ctrl_c_should_exit = false;
           line
         },
 
         Err(ReadlineError::Eof) => return Ok(()),
         Err(ReadlineError::Interrupted) => {
-          if should_exit {
+          if ctrl_c_should_exit {
             return Ok(());
           }
 
-          should_exit = true;
+          ctrl_c_should_exit = true;
           println!("(To exit, press Ctrl+C again or Ctrl+D or type :exit)");
           continue;
         },
@@ -88,24 +120,69 @@ impl RunArgs {
       };
 
       if line.trim().is_empty() {
-        continue;
+        continue; // Skip empty lines
       }
 
-      let line = text_data.alloc(line);
-      let eval_allocator = Allocator::new();
-      match executor.load_statement(&eval_allocator, line.as_str()) {
-        Ok(None) => {},
-        Ok(Some(expr)) => {
-          ABORT_EXECUTION.store(false, Ordering::Relaxed);
+      self.run_line(line);
+    }
+  }
 
-          let result = executor.evaluate_with_abort(&eval_allocator, expr, self.steps, &ABORT_EXECUTION);
-          match result {
-            None => println!("Interrupted"),
-            Some(result) => println!("{result:#}"),
-          }
-        },
-        Err(e) => println!("{e}"),
-      }
+  fn run_line(&mut self, line: String) {
+    // Check for built-in commands
+    let mut command_parts = line.split_whitespace();
+    match command_parts.next() {
+      Some(":h" | ":he" | ":hel" | ":help") => return self.print_help(),
+      Some(":s" | ":st" | ":ste" | ":step" | ":steps") => return self.set_steps(&line, command_parts.collect()),
+
+      // Unknown commands
+      None => {},
+      Some(_) => {},
+    }
+
+    // Not a built-in command, so run the line as code
+    let line = self.text_data.alloc(line);
+    let eval_allocator = Allocator::new();
+    match self.executor.load_statement(&eval_allocator, line.as_str()) {
+      Ok(None) => {},
+      Ok(Some(expr)) => {
+        self.abort.store(false, Ordering::Relaxed);
+
+        let result = self
+          .executor
+          .evaluate_with_abort(&eval_allocator, expr, self.show_steps, &self.abort);
+
+        match result {
+          None => println!("Interrupted"),
+          Some(result) => println!("{result:#}"),
+        }
+      },
+      Err(e) => println!("{e}"),
+    }
+  }
+
+  fn print_help(&self) {}
+
+  fn set_steps(&mut self, line: &str, args: Vec<&str>) {
+    match args.first().cloned() {
+      None => {
+        if self.show_steps {
+          println!("Reduction steps are {}", "on".green());
+        } else {
+          println!("Reduction steps are {}", "off".red());
+        }
+      },
+
+      Some("on" | "1" | "true") if args.len() == 1 => self.show_steps = true,
+
+      Some("off" | "0" | "false") if args.len() == 1 => self.show_steps = false,
+
+      Some(_) => {
+        println!(
+          "Expecting either '{}' or '{}', given '{line}'",
+          ":steps on".white().bold(),
+          ":steps off".white().bold(),
+        )
+      },
     }
   }
 }
